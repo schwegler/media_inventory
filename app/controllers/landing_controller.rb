f2a464107a997abc95da441125cf42050850de67
Andrew Schwegler <schwegler@gmail.com>
2026-06-06 12:40:20 -0500

    chore: clean up diagnostic endpoint

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -14,23 +14,15 @@ class LandingController < ApplicationController
     status = {
       database_connected: false,
       database_error: nil,
-      pending_migrations: nil,
       activities_count: nil,
       users_count: nil,
-      database_url: ENV['DATABASE_URL']&.gsub(/:[^@\/]+@/, ':FILTERED@')
+      database_url: ENV['DATABASE_URL']&.gsub(%r{:[^@/]+@}, ':FILTERED@')
     }
 
     begin
       status[:database_connected] = ActiveRecord::Base.connection.active?
       status[:activities_count] = Activity.count
       status[:users_count] = User.count
-      begin
-        ActiveRecord::Migration.check_pending!
-        status[:pending_migrations] = false
-      rescue ActiveRecord::PendingMigrationError => e
-        status[:pending_migrations] = true
-        status[:database_error] = e.message
-      end
     rescue StandardError => e
       status[:database_error] = "#{e.class}: #{e.message}"
     end
