ae5862ccb70901b6ce0719e0f5c2b063348a6e62
Andrew Schwegler <schwegler@gmail.com>
2026-06-06 23:31:40 -0500

    feat: Refactor API synchronization logic, fix RuboCop warnings, and wrap parameter lists

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -4,7 +4,9 @@ class ComicsController < InventoryController
   private
 
   def resource_params
-    params.require(:comic).permit(:title, :issue_number, :publisher, :writer, :artist, :is_public, :thumbnail_url,
-                                  :in_watchlist, :is_collected, :consumed, :consumed_at, :review, :rating, :cover_image)
+    params.require(:comic).permit(
+      :title, :issue_number, :publisher, :writer, :artist, :is_public, :thumbnail_url,
+      :in_watchlist, :is_collected, :consumed, :consumed_at, :review, :rating, :cover_image, :api_id
+    )
   end
 end
--- app/models/comic.rb
+++ app/models/comic.rb
@@ -17,30 +17,34 @@ class Comic < ApplicationRecord
     return if api_id.blank?
     return unless saved_change_to_api_id? || comic_issues.empty?
 
-    begin
-      require 'net/http'
-      require 'json'
-      url = URI("https://openlibrary.org/works/#{api_id}/editions.json?limit=50")
-      response = Net::HTTP.get(url)
-      data = JSON.parse(response)
-      editions = data['entries']
-      if editions.is_a?(Array)
-        comic_issues.delete_all
-        editions.each do |ed|
-          cover_id = ed['covers']&.first
-          cover_url = cover_id ? "https://covers.openlibrary.org/b/id/#{cover_id}-L.jpg" : nil
-          pub = ed['publishers']&.first
+    editions = fetch_editions_from_api
+    create_comic_issues(editions) if editions.is_a?(Array)
+  end
 
-          comic_issues.create!(
-            title: ed['title'],
-            release_date: ed['publish_date'],
-            publisher: pub,
-            thumbnail_url: cover_url
-          )
-        end
-      end
-    rescue StandardError => e
-      Rails.logger.error "Failed to sync Comic editions: #{e.message}"
+  def fetch_editions_from_api
+    require 'net/http'
+    require 'json'
+    url = URI("https://openlibrary.org/works/#{api_id}/editions.json?limit=50")
+    response = Net::HTTP.get(url)
+    JSON.parse(response)['entries']
+  rescue StandardError => e
+    Rails.logger.error "Failed to sync Comic editions: #{e.message}"
+    nil
+  end
+
+  def create_comic_issues(editions)
+    comic_issues.delete_all
+    editions.each do |ed|
+      cover_id = ed['covers']&.first
+      cover_url = cover_id ? "https://covers.openlibrary.org/b/id/#{cover_id}-L.jpg" : nil
+      pub = ed['publishers']&.first
+
+      comic_issues.create!(
+        title: ed['title'],
+        release_date: ed['publish_date'],
+        publisher: pub,
+        thumbnail_url: cover_url
+      )
     end
   end
 end
