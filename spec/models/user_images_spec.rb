facc41f198e7eae780066a56e45ba1dcee17a738
Andrew Schwegler <schwegler@gmail.com>
2026-06-19 22:37:41 -0500

    refactor: optimize collection and backlog item loading in user view and update avatar/banner test assets to SVG

--- spec/models/user_images_spec.rb
+++ spec/models/user_images_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe 'User Images', type: :model do
   let(:user) { User.create!(name: 'Image User', email: 'images@example.com', password: 'password') }
 
   it 'can have an avatar attached' do
-    user.avatar.attach(io: File.open(Rails.root.join('public', 'favicon.ico')), filename: 'favicon.ico',
-                       content_type: 'image/x-icon')
+    user.avatar.attach(io: File.open(Rails.root.join('public', 'favicon.svg')), filename: 'favicon.svg',
+                       content_type: 'image/svg+xml')
     expect(user.avatar).to be_attached
   end
 
   it 'can have a header banner attached' do
-    user.header_banner.attach(io: File.open(Rails.root.join('public', 'favicon.ico')), filename: 'favicon.ico',
-                              content_type: 'image/x-icon')
+    user.header_banner.attach(io: File.open(Rails.root.join('public', 'favicon.svg')), filename: 'favicon.svg',
+                              content_type: 'image/svg+xml')
     expect(user.header_banner).to be_attached
   end
 end
