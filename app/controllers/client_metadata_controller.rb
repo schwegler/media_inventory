d7c49294d5b91f8f07a757490f4acabcc364e8ee
Andrew Schwegler <schwegler@gmail.com>
2026-06-21 15:09:44 -0500

    refactor: remove redundant json format specification from client_id URL helper

--- app/controllers/client_metadata_controller.rb
+++ app/controllers/client_metadata_controller.rb
@@ -3,7 +3,7 @@
 class ClientMetadataController < ApplicationController
   def show
     render json: {
-      client_id: client_metadata_url(format: :json),
+      client_id: client_metadata_url,
       client_name: 'Media Inventory',
       redirect_uris: [url_for(controller: 'omniauth_callbacks', action: :atproto, provider: 'atproto', only_path: false)],
       scope: 'atproto transition:generic',
